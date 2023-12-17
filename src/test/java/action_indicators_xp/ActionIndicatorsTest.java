9874d16a1c5f7a8dfbc6de697674b89d7484176b
Vitor Mateus <vitor7jan@gmail.com>
2023-12-17 19:22:57 -0300

    test: get action indicators for VALE3

--- src/test/java/action_indicators_xp/ActionIndicatorsTest.java
+++ src/test/java/action_indicators_xp/ActionIndicatorsTest.java
@@ -5,22 +5,37 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import static org.junit.jupiter.api.Assertions.*;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+import java.util.stream.Collectors;
 
 public class ActionIndicatorsTest {
     private WebDriver driver;
     private String url = "https://stock-service-doj0.onrender.com/";
+    private Date initialDate;
+    private Date finalDate;
 
     public ActionIndicatorsTest(){
         setup();
     }
 
-    public void setup(){
+    private void setup(){
         System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver");
         ChromeOptions options = new ChromeOptions().addArguments("--remote-allow-origins=*");
         driver = new ChromeDriver(options);
+    }
+
+    private Date stringToDate(String input, String format) throws ParseException {
+        return new SimpleDateFormat(format, Locale.ENGLISH).parse(input);
     }
 
     @Given("I am in action indicators search page")
@@ -30,35 +45,70 @@ public class ActionIndicatorsTest {
 
     @When("insert {string} in action field")
     public void scenario1When(String action) {
+        driver.findElement(By.id("id_nome")).sendKeys(action);
     }
 
     @And("insert {string} in initial date field")
-    public void when(String initialDate) {
+    public void scenario1WhenAnd1(String initialDate) throws ParseException {
+        this.initialDate = stringToDate(initialDate, "MM-dd-yyyy");
+        driver.findElement(By.id("id_data_inicial")).sendKeys(initialDate);
     }
 
     @And("insert {string} in final date field")
-    public void scenario1WhenAnd1(String finalDate) {
+    public void scenario1WhenAnd2(String finalDate) throws ParseException {
+        this.finalDate = stringToDate(finalDate, "MM-dd-yyyy");
+        driver.findElement(By.id("id_data_final")).sendKeys(finalDate);
     }
 
     @And("click in submit button")
-    public void scenario1WhenAnd2() {
+    public void scenario1WhenAnd3() {
+        driver.findElement(By.cssSelector("button[type=submit]")).click();
     }
 
     @Then("the fields date, open, high, low, close and rsi should be in main table")
     public void scenario1Then() {
+        By mainTableHeadersSelector = By.cssSelector(".container > .table-responsive:nth-child(2) th");
+        List<String> mainTableHeaders = driver
+            .findElements(mainTableHeadersSelector)
+            .stream()
+            .map(th -> th.getText().toLowerCase())
+            .collect(Collectors.toList());
+        assertTrue(mainTableHeaders.contains("date"));
+        assertTrue(mainTableHeaders.contains("open"));
+        assertTrue(mainTableHeaders.contains("high"));
+        assertTrue(mainTableHeaders.contains("low"));
+        assertTrue(mainTableHeaders.contains("close"));
+        assertTrue(mainTableHeaders.contains("rsi"));
     }
 
     @And("first table line date should be greater than or equal initial date")
-    public void scenario1ThenAnd1() {
+    public void scenario1ThenAnd1() throws ParseException {
+        By firstLineDateSelector = By.cssSelector(".container > .table-responsive:nth-child(3) tr:first-child td:first-child");
+        WebElement firstLineDateElement = driver.findElement(firstLineDateSelector);
+        if(firstLineDateElement == null) return;
+        Date firstLineDate = stringToDate(firstLineDateElement.getText(), "dd-MM-yyyy");
+        assertTrue(firstLineDate.compareTo(this.initialDate) >= 0);
     }
 
     @And("last table line date should be less than or equal final date")
-    public void scenario1ThenAnd2() {
+    public void scenario1ThenAnd2() throws ParseException {
+        By lastLineDateSelector = By.cssSelector(".container > .table-responsive:nth-child(3) tr:last-child td:first-child");
+        WebElement lastLineDateElement = driver.findElement(lastLineDateSelector);
+        Date lastLineDate = stringToDate(lastLineDateElement.getText(), "dd-MM-yyyy");
+        assertTrue(lastLineDate.compareTo(this.finalDate) <= 0);
     }
 
     @And("the fields medium return, volatility and var should be in top table")
     public void scenario1ThenAnd3() {
-
+        By topTableHeadersSelector = By.cssSelector("#content .table-responsive th");
+        List<String> topTableHeaders = driver
+            .findElements(topTableHeadersSelector)
+            .stream()
+            .map(th -> th.getText().toLowerCase())
+            .collect(Collectors.toList());
+        assertTrue(topTableHeaders.contains("retorno médio"));
+        assertTrue(topTableHeaders.contains("volatilidade"));
+        assertTrue(topTableHeaders.contains("var (value-at-risk)"));
     }
 
     @Given("I am in action indicators search page 2")
--- src/test/resources/action_indicators_xp/action_indicators.feature
+++ src/test/resources/action_indicators_xp/action_indicators.feature
@@ -3,8 +3,8 @@ Feature: action indicators
   Scenario: get action indicators for VALE3
     Given I am in action indicators search page
     When insert "VALE3" in action field
-      And insert "01-02-2023" in initial date field
-      And insert "01-06-2023" in final date field
+      And insert "02-01-2023" in initial date field
+      And insert "06-01-2023" in final date field
       And click in submit button
     Then the fields date, open, high, low, close and rsi should be in main table
       And first table line date should be greater than or equal initial date
